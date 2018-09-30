62de9a3990961ce0580a0348afa27a131651c0bb
luya0000 <1q2w3e4r>
2018-09-30 23:23:20 +0800

    ［～］　制作完成。

--- backend/src/main/database/database.sql
+++ backend/src/main/database/database.sql
@@ -21,16 +21,37 @@ DROP TABLE IF EXISTS `sys_depart`;
 
 CREATE TABLE `sys_depart` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '表id',
-  `name` varchar(100) DEFAULT NULL COMMENT '部门名称',
-  `desc` varchar(256) DEFAULT NULL COMMENT '部门说明',
+  `code` varchar(32) NOT NULL COMMENT '关联文件存放路径',
+  `menu_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL COMMENT '部门名称',
+  `note` varchar(256) DEFAULT NULL COMMENT '部门说明',
   `update_user` varchar(20) DEFAULT NULL,
   `update_time` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `NewIndex1` (`code`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 
 /*Data for the table `sys_depart` */
 
-insert  into `sys_depart`(`id`,`name`,`desc`,`update_user`,`update_time`) values (1,'三总师','三总师',NULL,NULL),(2,'办公室','办公室',NULL,NULL),(3,'人力资源部','人力资源部',NULL,NULL),(4,'财务部','财务部',NULL,NULL),(5,'安全生产部','安全生产部',NULL,NULL),(6,'经营发展部','经营发展部',NULL,NULL);
+insert  into `sys_depart`(`id`,`code`,`menu_id`,`name`,`note`,`update_user`,`update_time`) values (1,'SYSTEM',1,'系统部门','系统部门','admin','2018-09-27 14:52:04');
+
+/*Table structure for table `sys_file` */
+
+DROP TABLE IF EXISTS `sys_file`;
+
+CREATE TABLE `sys_file` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(512) NOT NULL COMMENT '文件名',
+  `dep_id` int(11) NOT NULL COMMENT '部门id',
+  `path` varchar(1024) NOT NULL COMMENT '文件位置',
+  `owner` varchar(20) NOT NULL COMMENT 'userId',
+  `type` varchar(11) NOT NULL COMMENT '文件类型',
+  `size` float NOT NULL COMMENT '文件大小',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_file` */
 
 /*Table structure for table `sys_menu` */
 
@@ -47,26 +68,13 @@ CREATE TABLE `sys_menu` (
   `status` int(4) DEFAULT NULL COMMENT '菜单状态',
   `update_user` varchar(20) DEFAULT NULL,
   `update_time` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `NewIndex1` (`name`,`parent_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
 
 /*Data for the table `sys_menu` */
 
-insert  into `sys_menu`(`id`,`parent_id`,`name`,`icon`,`url`,`type`,`order`,`status`,`update_user`,`update_time`) values (1,0,'系统管理','&#xe610','#',1,1,1,NULL,NULL),(2,1,'角色管理','&#xe604','#/view/role',1,2,1,NULL,NULL),(3,1,'部门管理','&#xe604','#/view/depart',1,3,1,NULL,NULL),(4,1,'菜单管理','&#xe610','#/view/menu',1,4,1,NULL,NULL),(5,1,'人员管理','&#xe604','#/view/user',1,5,1,NULL,NULL),(6,1,'密码修改','&#xe604','#/view/changepwd',1,6,1,NULL,NULL),(10,0,'文档管理','&#xe610','#',2,7,1,NULL,NULL),(11,10,'三总师','&#xe604','#/view/file_manage',2,8,1,NULL,NULL),(12,10,'办公室','&#xe604','#/view/file_manage',2,9,1,NULL,NULL),(13,10,'人力资源部','&#xe604','#/view/file_manage',2,10,1,NULL,NULL),(14,10,'财务部','&#xe604','#/view/file_manage',2,11,1,NULL,NULL),(15,10,'安全生产部','&#xe604','#/view/file_manage',2,12,1,NULL,'0000-00-00 00:00:00');
-
-/*Table structure for table `sys_menu_depart` */
-
-DROP TABLE IF EXISTS `sys_menu_depart`;
-
-CREATE TABLE `sys_menu_depart` (
-  `menu_id` int(11) NOT NULL COMMENT '角色id',
-  `depart_id` int(11) NOT NULL COMMENT '部门id',
-  PRIMARY KEY (`menu_id`,`depart_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `sys_menu_depart` */
-
-insert  into `sys_menu_depart`(`menu_id`,`depart_id`) values (11,1),(12,2),(13,3),(14,4),(15,5);
+insert  into `sys_menu`(`id`,`parent_id`,`name`,`icon`,`url`,`type`,`order`,`status`,`update_user`,`update_time`) values (1,0,'系统管理','&#xe610','#',1,1,1,NULL,'2018-09-22 15:01:47'),(2,1,'角色管理','&#xe604','#/view/role',1,2,1,NULL,'2018-09-22 15:01:47'),(3,1,'部门管理','&#xe604','#/view/depart',1,3,1,NULL,'2018-09-22 15:01:47'),(4,1,'人员管理','&#xe604','#/view/user',1,5,1,NULL,'2018-09-22 15:01:47'),(10,0,'文档管理','&#xe610','#',2,1,1,NULL,'2018-09-22 15:01:47');
 
 /*Table structure for table `sys_permission` */
 
@@ -74,16 +82,16 @@ DROP TABLE IF EXISTS `sys_permission`;
 
 CREATE TABLE `sys_permission` (
   `id` int(11) NOT NULL COMMENT '权限Id',
-  `desc` varchar(100) DEFAULT NULL COMMENT '权限描述',
-  `name` varchar(100) DEFAULT NULL COMMENT '权限名称',
-  `menu_id` int(11) DEFAULT NULL COMMENT '菜单Id',
+  `note` varchar(100) DEFAULT NULL COMMENT '权限描述',
+  `name` varchar(100) NOT NULL COMMENT '权限名称',
+  `perm_type` int(11) DEFAULT NULL COMMENT '0:普通人员独有权限，5：普通管理员独有的权限，9：系统管理员独有权限',
   PRIMARY KEY (`id`),
-  KEY `p_fk_1` (`menu_id`)
+  KEY `p_fk_1` (`perm_type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `sys_permission` */
 
-insert  into `sys_permission`(`id`,`desc`,`name`,`menu_id`) values (1,'用户管理的权限','用户管理',1),(2,'管理员管理的权限','管理员管理',2),(3,'用户统计的权限','用户统计',3),(4,'在线管理的权限','在线管理',4),(5,'在线情况的权限','在线情况',5),(6,'在线聊天的权限','在线聊天',6),(7,'系统管理的权限','系统管理',7),(8,'角色管理的权限','角色管理',8),(9,'权限管理的权限','权限管理',9),(10,'菜单管理的权限','菜单管理',10),(11,'平台资料的权限','平台资料',11);
+insert  into `sys_permission`(`id`,`note`,`name`,`perm_type`) values (1,'查询文件列表','SEARCH_LIST',0),(2,'上传文件权限','UP_FILE',0),(3,'下载文件权限','DOWN_FILE',0),(4,'删除文件权限','DEL_FILE',0),(5,'普通人员管理权限','DEPART_ADMIN',5),(6,'人员管理的权限','USER_MANAGE',9),(7,'角色管理的权限','ROLE_MANAGE',9),(8,'部门管理的权限','DEPART_MANAGE',9),(10,'菜单管理的权限','MENU_MANAGE',9);
 
 /*Table structure for table `sys_role` */
 
@@ -91,16 +99,31 @@ DROP TABLE IF EXISTS `sys_role`;
 
 CREATE TABLE `sys_role` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '角色Id',
-  `name` varchar(100) DEFAULT NULL COMMENT '角色名称',
-  `desc` varchar(100) DEFAULT NULL COMMENT '角色描述',
+  `depart_id` int(11) DEFAULT NULL COMMENT '角色属于部门',
+  `name` varchar(100) NOT NULL COMMENT '角色名称',
+  `note` varchar(100) DEFAULT NULL COMMENT '角色描述',
   `update_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
   `update_user` varchar(20) DEFAULT NULL COMMENT '创建人',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 /*Data for the table `sys_role` */
 
-insert  into `sys_role`(`id`,`name`,`desc`,`update_time`,`update_user`) values (1,'超级管理员','超级管理员拥有所有权限','2018-09-14 23:59:00',NULL),(2,'部门管理员','用户管理权限','2018-09-14 23:59:02',NULL),(3,'部门成员','角色管理权限','2018-09-14 23:59:03',NULL);
+insert  into `sys_role`(`id`,`depart_id`,`name`,`note`,`update_time`,`update_user`) values (1,1,'超级管理员','超级管理员拥有所有权限','2018-09-28 14:20:12','admin');
+
+/*Table structure for table `sys_role_depart_drop` */
+
+DROP TABLE IF EXISTS `sys_role_depart_drop`;
+
+CREATE TABLE `sys_role_depart_drop` (
+  `role_id` int(11) NOT NULL COMMENT '角色id',
+  `depart_id` int(11) NOT NULL COMMENT '部门id',
+  PRIMARY KEY (`role_id`,`depart_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_role_depart_drop` */
+
+insert  into `sys_role_depart_drop`(`role_id`,`depart_id`) values (1,1);
 
 /*Table structure for table `sys_role_menu` */
 
@@ -109,12 +132,13 @@ DROP TABLE IF EXISTS `sys_role_menu`;
 CREATE TABLE `sys_role_menu` (
   `role_id` int(11) NOT NULL,
   `menu_id` int(11) NOT NULL,
-  PRIMARY KEY (`role_id`,`menu_id`)
+  PRIMARY KEY (`role_id`,`menu_id`),
+  UNIQUE KEY `role_menu_id` (`role_id`,`menu_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `sys_role_menu` */
 
-insert  into `sys_role_menu`(`role_id`,`menu_id`) values (1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),(1,9),(1,10),(1,11),(2,10),(2,11);
+insert  into `sys_role_menu`(`role_id`,`menu_id`) values (1,1),(1,2),(1,3),(1,4),(1,10);
 
 /*Table structure for table `sys_role_permission` */
 
@@ -127,7 +151,7 @@ CREATE TABLE `sys_role_permission` (
 
 /*Data for the table `sys_role_permission` */
 
-insert  into `sys_role_permission`(`role_id`,`pm_id`) values (2,1),(1,1),(1,2),(1,3),(1,4),(9,1),(1,7),(9,2),(2,2),(1,10),(1,9),(1,8),(2,3),(1,6),(1,5);
+insert  into `sys_role_permission`(`role_id`,`pm_id`) values (1,1),(1,2),(1,3),(1,4);
 
 /*Table structure for table `sys_user` */
 
@@ -152,7 +176,7 @@ CREATE TABLE `sys_user` (
 
 /*Data for the table `sys_user` */
 
-insert  into `sys_user`(`user_id`,`user_name`,`password`,`phone`,`sex`,`email`,`note`,`rank`,`question`,`answer`,`status`,`update_time`,`update_user`) values ('admin','admin','$2a$10$aalHYXvh1tcNIJoKARxtC.kUUWlKcUys.yJPeA1k/IBJQ1o1agLyq','1552323312','男','313222@foxmail.com','超级管理员','admin','2017-08-12','127.0.0.1',0,NULL,''),('sys','sys','$2a$10$aalHYXvh1tcNIJoKARxtC.kUUWlKcUys.yJPeA1k/IBJQ1o1agLyq','1552323312','男','313222@foxmail.com','系统管理员','sys','2017-08-25','127.0.0.1',0,NULL,''),('test','test','123','12332233212','保密','2312@qq.com','没有备注','user','2017-11-25','127.0.0.1',NULL,NULL,''),('user','user','adf8e0d0828bde6e90c2bab72e7a2a763d88a0de','1552323312','男','313222@foxmail.com','用户','user','2017-08-18','127.0.0.1',0,NULL,'');
+insert  into `sys_user`(`user_id`,`user_name`,`password`,`phone`,`sex`,`email`,`note`,`rank`,`question`,`answer`,`status`,`update_time`,`update_user`) values ('admin','admin','$2a$10$aalHYXvh1tcNIJoKARxtC.kUUWlKcUys.yJPeA1k/IBJQ1o1agLyq','1552323312','1','313222@foxmail.com','超级管理员','admin','2017-08-12','127.0.0.1',0,'2018-09-22 15:01:47','');
 
 /*Table structure for table `sys_user_role` */
 
@@ -166,7 +190,7 @@ CREATE TABLE `sys_user_role` (
 
 /*Data for the table `sys_user_role` */
 
-insert  into `sys_user_role`(`user_id`,`role_id`) values ('admin',1),('admin',2);
+insert  into `sys_user_role`(`user_id`,`role_id`) values ('admin',1);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
